e42f8abc7319c3ba40d48f10c53edf3d66a6ba72
Steffan Davies <steff@steffandroid.co.uk>
2017-03-19 15:48:33 +0000

    Remove modules from @Subcomponent

--- app/src/main/java/uk/co/steffandroid/daggerstackoverflowexample/ui/MainComponent.java
+++ app/src/main/java/uk/co/steffandroid/daggerstackoverflowexample/ui/MainComponent.java
@@ -3,7 +3,7 @@ package uk.co.steffandroid.daggerstackoverflowexample.ui;
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
 
-@Subcomponent(modules = MainModule.class)
+@Subcomponent
 public interface MainComponent extends AndroidInjector<MainActivity> {
     @Subcomponent.Builder
     abstract class Builder extends AndroidInjector.Builder<MainActivity> {
